1c86607fe8f5698c2d4e47f851c9b73cfb6aa7a3
KALLYCHURN DOOSHYANT RAI <TP050544@mail.apu.edu.my>
2021-07-28 13:56:55 +0800

    making table structure of Promo defining column needed and to generate crud table

---
+++ SpeedPort-Airlines/Models/Promo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpeedPort_Airlines.Models
+{
+    public class Promo //making table structure of Promo
+    {
+        //defining column needed and to generate crud table
+
+        public int ID {get; set;}
+
+        public string DestinationCountry { get; set; }
+
+        public DateTime PromoValidity { get; set; }
+
+        public string TravelAgency { get; set; }
+
+        public decimal PromoPrice { get; set; }
+    }
+}
