4a8e9d190dcfbbfc23137e77ede3ed14da8ff55f
KALLYCHURN DOOSHYANT RAI <TP050544@mail.apu.edu.my>
2021-07-28 15:42:39 +0800

    added Models/BookPromoSlot class to be used for service bus. To note that no migration has been done yet to this models class

---
+++ SpeedPort-Airlines/Migrations/SpeedPort_AirlinesNew/20210728061207_createPromoTable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SpeedPort_Airlines.Migrations.SpeedPort_AirlinesNew
+{
+    public partial class createPromoTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Promo",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DestinationCountry = table.Column<string>(nullable: true),
+                    PromoValidity = table.Column<DateTime>(nullable: false),
+                    TravelAgency = table.Column<string>(nullable: true),
+                    PromoPrice = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Promo", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Promo");
+        }
+    }
+}
---
+++ SpeedPort-Airlines/Models/BookPromoSlot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpeedPort_Airlines.Models
+{
+    public class BookPromoSlot
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Travel Agency")]
+        public string TravelAgencyName { get; set; }
+
+        [Display(Name = "Destination Country")]
+        public string DestinationCountryName { get; set; }
+
+        [Display(Name = "Special Flight Deal")]
+        public string SpecialFlightDeal { get; set; }
+    }
+}
