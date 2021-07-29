6d26dd6d207964df958678bbc1491e0dcd5f9420
KALLYCHURN DOOSHYANT RAI <TP050544@mail.apu.edu.my>
2021-07-30 03:22:53 +0800

    View and search data in table

--- SpeedPort-Airlines/Controllers/TablesController.cs
+++ SpeedPort-Airlines/Controllers/TablesController.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using SpeedPort_Airlines.Models;
 
 namespace SpeedPort_Airlines.Controllers
 {
@@ -40,6 +41,210 @@ namespace SpeedPort_Airlines.Controllers
 
             //display the create table result
             return View();
+        }
+
+        public ActionResult InsertSingleData()
+        {
+            CloudTable table = getStorageAccountInformation();
+
+            //object for DestinationCountryEntity 1
+            DestinationCountryEntity SpecialFlightDeal1 = new DestinationCountryEntity("England", "London");
+            SpecialFlightDeal1.Season = "Summer";
+            SpecialFlightDeal1.PromoExpiry = new DateTime (2021, 6, 01);
+            SpecialFlightDeal1.PromoCode = "FLYLONDON22";
+
+            try
+            {
+                //insert operation here
+                TableOperation insert = TableOperation.Insert(SpecialFlightDeal1);
+                TableResult result = table.ExecuteAsync(insert).Result;
+
+                //get the result answer (success / not success to insert)
+                ViewBag.TableName = table.Name;
+                ViewBag.result = result.HttpStatusCode;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error Message: " + ex.ToString());
+            }
+
+            //object for DestinationCountryEntity 2
+            DestinationCountryEntity SpecialFlightDeal2 = new DestinationCountryEntity("France", "Paris");
+            SpecialFlightDeal2.Season = "Summer";
+            SpecialFlightDeal2.PromoExpiry = new DateTime(2021, 11, 01);
+            SpecialFlightDeal2.PromoCode = "VOYAGEPARIS11";
+
+            try
+            {
+                //insert operation here
+                TableOperation insert = TableOperation.Insert(SpecialFlightDeal1);
+                TableResult result = table.ExecuteAsync(insert).Result;
+
+                //get the result answer (success / not success to insert)
+                ViewBag.TableName = table.Name;
+                ViewBag.result = result.HttpStatusCode;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error Message: " + ex.ToString());
+            }
+
+            //object for DestinationCountryEntity 3
+            DestinationCountryEntity SpecialFlightDeal3 = new DestinationCountryEntity("Mauritius", "Port Louis");
+            SpecialFlightDeal3.Season = "Summer";
+            SpecialFlightDeal3.PromoExpiry = new DateTime(2021, 10, 01);
+            SpecialFlightDeal3.PromoCode = "FLYMRU33";
+
+            try
+            {
+                //insert operation here
+                TableOperation insert = TableOperation.Insert(SpecialFlightDeal1);
+                TableResult result = table.ExecuteAsync(insert).Result;
+
+                //get the result answer (success / not success to insert)
+                ViewBag.TableName = table.Name;
+                ViewBag.result = result.HttpStatusCode;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error Message: " + ex.ToString());
+            }
+
+            //object for DestinationCountryEntity 4
+            DestinationCountryEntity SpecialFlightDeal4 = new DestinationCountryEntity("Japan", "Tokyo");
+            SpecialFlightDeal4.Season = "Winter";
+            SpecialFlightDeal4.PromoExpiry = new DateTime(2022, 02, 01);
+            SpecialFlightDeal4.PromoCode = "FLYJPN44";
+
+            try
+            {
+                //insert operation here
+                TableOperation insert = TableOperation.Insert(SpecialFlightDeal1);
+                TableResult result = table.ExecuteAsync(insert).Result;
+
+                //get the result answer (success / not success to insert)
+                ViewBag.TableName = table.Name;
+                ViewBag.result = result.HttpStatusCode;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error Message: " + ex.ToString());
+            }
+            return View();
+
+        }
+        public ActionResult ViewSpecialFlightDeals()
+        {
+            string errormessage = null;
+            CloudTable table = getStorageAccountInformation();
+            try
+            {
+                TableQuery<DestinationCountryEntity> query = new TableQuery<DestinationCountryEntity>();
+                List<DestinationCountryEntity> countries = new List<DestinationCountryEntity>();
+                TableContinuationToken token = null; //to identify still have next data or not
+
+                do
+                {
+                    TableQuerySegment<DestinationCountryEntity> result = table.ExecuteQuerySegmentedAsync(query, token).Result; //execute the query
+                    token = result.ContinuationToken;
+
+                    foreach (DestinationCountryEntity country in result.Results) // to retrieve every single data in one row
+                    {
+                        countries.Add(country);
+                    }
+                }
+                while (token != null);
+
+                if (countries.Count != 0)
+                {
+                    return View(countries); //back to display
+                }
+                else
+                {
+                    errormessage = "Data not found!";
+                    return RedirectToAction("SearchPage", "Tables", new { dialogmsg = errormessage });
+                }
+
+            }
+            catch (Exception ex)
+            {
+                errormessage = "Technical Issue: " + ex.ToString(); // technical error
+            }
+            return RedirectToAction("SearchPage", "Tables", new { dialogmsg = errormessage });
+        }
+
+        public ActionResult SearchPage(string dialogmsg = null)
+        {
+            ViewBag.msg = dialogmsg;
+            return View();
+        }
+
+        //get the single data from the table storage
+        public ActionResult getSingleEntity(string PartitionName, string RowName)
+        {
+            string errormessage = "";
+            CloudTable table = getStorageAccountInformation();
+            try
+            {
+                TableOperation retrieveprocess = TableOperation.Retrieve<DestinationCountryEntity>(PartitionName, RowName);
+                TableResult result = table.ExecuteAsync(retrieveprocess).Result;
+                if (result.Etag != null)
+                {
+                    return View(result); //if got result, bring the data to a new page called getsinglentity page
+                }
+                else
+                {
+                    errormessage = "Data not found in the table!"; //data not found answer
+                }
+            }
+
+            catch (Exception ex)
+            {
+                errormessage = "Technical issue: " + ex.ToString();
+            }
+            return RedirectToAction("SearchPage", "Tables", new { dialogmsg = errormessage });
+        }
+
+        public ActionResult getGroupEntity(string PartitionName)
+        {
+            string errormessage = null;
+            CloudTable table = getStorageAccountInformation();
+            try
+            {
+                TableQuery<DestinationCountryEntity> query = new TableQuery<DestinationCountryEntity>()
+                            .Where(TableQuery.GenerateFilterCondition("PartitionKey",
+                             QueryComparisons.Equal, PartitionName));
+                List<DestinationCountryEntity> countries = new List<DestinationCountryEntity>();
+                TableContinuationToken token = null; //to identify still have next data or not
+
+                do
+                {
+                    TableQuerySegment<DestinationCountryEntity> result = table.ExecuteQuerySegmentedAsync(query, token).Result; //execute the query
+                    token = result.ContinuationToken;
+
+                    foreach (DestinationCountryEntity country in result.Results) // to retrieve every single data in one row
+                    {
+                        countries.Add(country);
+                    }
+                }
+                while (token != null);
+
+                if (countries.Count != 0)
+                {
+                    return View(countries); //back to display
+                }
+                else
+                {
+                    errormessage = "Data not found!";
+                    return RedirectToAction("SearchPage", "Tables", new { dialogmsg = errormessage });
+                }
+
+            }
+            catch (Exception ex)
+            {
+                errormessage = "Technical Issue: " + ex.ToString(); // technical error
+            }
+            return RedirectToAction("SearchPage", "Tables", new { dialogmsg = errormessage });
         }
     }
 }
---
+++ SpeedPort-Airlines/Models/DestinationCountryEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace SpeedPort_Airlines.Models
+{
+    public class DestinationCountryEntity: TableEntity //partitionkey, rowkey
+    {
+        public DestinationCountryEntity(string Country, string City)
+        {
+            this.PartitionKey = Country;
+            this.RowKey = City;
+        }
+        public DestinationCountryEntity() { }
+
+        public String Season { get; set; }
+        
+        public DateTime  PromoExpiry { get; set; }
+
+        public String PromoCode { get; set; }
+    }
+}
+
