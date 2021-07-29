9561cf64e357a9a2b61b423135da0cf35607ee30
KALLYCHURN DOOSHYANT RAI <TP050544@mail.apu.edu.my>
2021-07-30 00:28:21 +0800

    Created Table

--- SpeedPort-Airlines/Controllers/TablesController.cs
+++ SpeedPort-Airlines/Controllers/TablesController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +14,31 @@ namespace SpeedPort_Airlines.Controllers
     {
         public IActionResult Index()
         {
+            return View();
+        }
+
+        private CloudTable getStorageAccountInformation()
+        {
+            //1.1 link with the appsettings.json to get the accesskey
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json");
+            IConfigurationRoot configure = builder.Build();
+            CloudStorageAccount accountobject = CloudStorageAccount.Parse(configure["ConnectionStrings:blobstorageconnection"]);
+
+            //1.2 create/link to related table
+            CloudTableClient tableclient = accountobject.CreateCloudTableClient();
+            CloudTable table = tableclient.GetTableReference("TestTable");
+            return table;
+        }
+
+        public ActionResult CreateTable()
+        {
+            CloudTable table = getStorageAccountInformation();
+            //create table if table not yet exist
+            ViewBag.Success = table.CreateIfNotExistsAsync().Result;
+            ViewBag.tablename = table.Name;
+
+            //display the create table result
             return View();
         }
     }
