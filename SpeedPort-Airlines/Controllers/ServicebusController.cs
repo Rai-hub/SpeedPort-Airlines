a969ef65ffe1658755b5c0dde23be8aeb1f3d083
KALLYCHURN DOOSHYANT RAI <TP050544@mail.apu.edu.my>
2021-07-30 19:21:29 +0800

    Added Servicebus controller and added azure servicebus dependencies

---
+++ SpeedPort-Airlines/Controllers/ServicebusController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Management;
+
+namespace SpeedPort_Airlines.Controllers
+{
+    public class ServicebusController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
