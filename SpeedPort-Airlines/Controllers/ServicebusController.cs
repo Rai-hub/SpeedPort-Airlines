08434be466f7408b187f19a3ca13d3265f29aca8
KALLYCHURN DOOSHYANT RAI <TP050544@mail.apu.edu.my>
2021-07-30 19:30:06 +0800

    added servicebusconnection string and queue name

--- SpeedPort-Airlines/Controllers/ServicebusController.cs
+++ SpeedPort-Airlines/Controllers/ServicebusController.cs
@@ -10,6 +10,9 @@ namespace SpeedPort_Airlines.Controllers
 {
     public class ServicebusController : Controller
     {
+        const string ServiceBusConnectionString = "Endpoint=sb://servicebusspeedportairlines.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=NkrTwbzG1EIoQz4YNnd29fjDw+z38O1bgHSCnSOb8HM="; 
+        const string QueueName = "speedportqueue";
+
         public IActionResult Index()
         {
             return View();
